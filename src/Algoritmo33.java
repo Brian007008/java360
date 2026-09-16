ca3f1bd38c1cf51c9c1dcdce012f01f59962e765
= <=>
2026-09-16 11:45:49 -0300

    save

--- src/Algoritmo33.java
+++ src/Algoritmo33.java
@@ -1,3 +1,21 @@
 public class Algoritmo33 {
+
+    private int chave;
+
+    public void inserirChave(int c){
+        chave = c;
+    }
+    public int retornarChave(){
+        return chave;
+    }
+    public String abrirPorta(){
+        String resposta;
+        if(chave ==7){
+            resposta = "A porta verde agua se abre;";
+        }else{
+            resposta = "A porta de cobre se abre";
+        }
+        return resposta;
+    }
     
 }
--- src/Principal.java
+++ src/Principal.java
@@ -12,5 +12,11 @@ public class Principal {
 
         Algoritmo32 objeto3 = new Algoritmo32();
         IO.println(objeto3.mostrarSala("Maria Eduarda"));
+
+        Algoritmo33 objeto4 = new Algoritmo33();
+        objeto4.inserirChave(7);
+        IO.println(objeto4.retornarChave());
+        IO.println(objeto4.abrirPorta());
 }
+
   }
